0c8b7d266b46a8115c67e9398f50d3a0efc5e22a
KAPIROTU <70527279+KAPIROTU@users.noreply.github.com>
2024-11-14 13:06:48 -0300

    Update app.py
    Update para validar conexão com mongodb e flask

--- app.py
+++ app.py
@@ -1,15 +1,23 @@
-from flask import Flask, request, jsonify
+import os
+import sys  # Adicione esta linha
+from pymongo import MongoClient
+from flask import Flask
+
+# Verifique se a variável de ambiente 'MONGO_URI' está definida
+mongo_uri = os.getenv('MONGO_URI')
+if mongo_uri is None:
+    print("Erro: A variável de ambiente 'MONGO_URI' não está definida.")
+    sys.exit(1)  # Encerra a aplicação caso não tenha a URI do MongoDB
+
+client = MongoClient(mongo_uri)  # Conexão com o MongoDB
+
+# Criando a aplicação Flask
 app = Flask(__name__)
 
 @app.route('/')
-def home():
-    return 'Aplicação rodando!', 200
+def index():
+    return "Hello, World!"
 
-@app.route('/webhook', methods=['POST'])
-def webhook():
-    data = request.json
-    # Processar dados do webhook aqui
-    return jsonify({'status': 'received'}), 200
-
-if __name__ == '__main__':
+if __name__ == "__main__":
+    # Rodando a aplicação com Gunicorn
     app.run(host='0.0.0.0', port=5000)
